46496eb28f621c7eb58c2743a3653f9e5ca0db57
Leondro Lio <12liol@gravesendgrammar.eu>
2018-06-12 22:56:50 +0000

    Complete the Dance Dance Layer (DDL)

--- sql/ThreeSeasonsHotel/basic.sql
+++ sql/ThreeSeasonsHotel/basic.sql
@@ -1,12 +1,14 @@
 USE TSH;
 
 -- Clean the database
+SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE hotels;
 DROP TABLE rooms;
 DROP TABLE employees;
 DROP TABLE roomBookings;
 DROP TABLE bookings;
 DROP TABLE customers;
+SET FOREIGN_KEY_CHECKS = 1;
 
 -- Create tables
 CREATE TABLE hotels (
@@ -19,32 +21,22 @@ CREATE TABLE hotels (
 
 CREATE TABLE rooms (
     ID          INT               PRIMARY KEY AUTO_INCREMENT,
+    HotelID     INT,
     Floor       VARCHAR(3),
     Class       INT, -- Enum
-    HotelID     INT
+    FOREIGN KEY (HotelID) REFERENCES hotels(ID)
 );
 
 CREATE TABLE employees (
     ID          INT               PRIMARY KEY AUTO_INCREMENT,
+    HotelID     INT,
     Title       INT, -- Enum
     Surname     VARCHAR(255),
     OtherNames  VARCHAR(255),
     NationalInsurance   VARCHAR(10),
     Joined      DATE,
-    Salary      INT
-);
-
-CREATE TABLE roomBookings (
-    ID          INT               PRIMARY KEY AUTO_INCREMENT,
-    RoomID      INT,
-    BookingID   INT
-);
-
-CREATE TABLE bookings (
-    ID          INT               PRIMARY KEY AUTO_INCREMENT,
-    CustomerID  INT,
-    Price       INT,
-    Created     DATE
+    Salary      INT,
+    FOREIGN KEY (HotelID) REFERENCES hotels(ID)
 );
 
 CREATE TABLE customers (
@@ -57,4 +49,22 @@ CREATE TABLE customers (
     City        VARCHAR(255),
     Country     VARCHAR(2),
     PostZipCode VARCHAR(255)
+);
+
+
+CREATE TABLE bookings (
+    ID          INT               PRIMARY KEY AUTO_INCREMENT,
+    CustomerID  INT,
+    Price       INT,
+    Created     DATE,
+    FOREIGN KEY (CustomerID) REFERENCES customers(ID)
+);
+
+
+CREATE TABLE roomBookings (
+    ID          INT               PRIMARY KEY AUTO_INCREMENT,
+    RoomID      INT,
+    BookingID   INT,
+    FOREIGN KEY (RoomID) REFERENCES rooms(ID),
+    FOREIGN KEY (BookingID) REFERENCES bookings(ID)
 );
