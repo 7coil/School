35dccb185eb6e38ac68a03a32050cabbb5e4d7c3
Leondro Lio <admin@moustacheminer.com>
2018-06-19 11:33:38 +0100

    Add more features to top trumps

--- sql/TopTrumps/StrongButNotSpecial.sql
+++ sql/TopTrumps/StrongButNotSpecial.sql
@@ -1,1 +1,3 @@
-SELECT * FROM toptrumps.characters WHERE strength > 15 AND "special powers" < 5 ORDER BY ("Special Powers") ASC LIMIT 3;
+USE toptrumps;
+
+SELECT * FROM characters WHERE strength > 15 AND "special powers" < 5 ORDER BY ("Special Powers") ASC LIMIT 3;
--- sql/TopTrumps/averages.sql
+++ sql/TopTrumps/averages.sql
@@ -1,6 +1,22 @@
+-- Use the correct database
 USE toptrumps;
 
+-- Get the maximum, minimum and average special powers
 SELECT
 (SELECT Avg(`special powers`) FROM characters) AS "Average Special Powers",
 (SELECT Max(`special powers`) FROM characters) AS "Maximum Special Powers",
 (SELECT Min(`special powers`) FROM characters) AS "Minimum Special Powers";
+
+-- Get the character with the lowest special powers
+SELECT * FROM characters ORDER BY `special powers` ASC LIMIT 1;
+
+-- Get the Villain with the highest special powers
+SELECT * FROM characters WHERE side LIKE "Villain" ORDER BY `special powers` DESC LIMIT 1;
+
+-- Get all characters who debuted in a year
+SELECT * FROM characters WHERE `debut year` < 1960;
+
+-- Get the number of characters
+SELECT
+(SELECT SUM(cunning) FROM characters WHERE side LIKE "Villain") AS "Villain Cunning",
+(SELECT SUM(cunning) FROM characters WHERE side NOT LIKE "Villain") AS "Hero Cunning";
